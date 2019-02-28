cbceaa6618071d0ef7d9bf7b3769b60a666d94b6
A Belgrave <abelgrave95@gmail.com>
2019-02-28 16:57:30 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -26,11 +26,11 @@ end
 def get_average_age_for_season(data, season)
   age = 0
   contestants = 0
-  data.each do |season, info| 
+  data.each do |season, info|
     contestants = info.length
     info.each { |person, v| age += person["age"].to_f }
   end
 
   age/contestants.round(0)
-  
+
 end
